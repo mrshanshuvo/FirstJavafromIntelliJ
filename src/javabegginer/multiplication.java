0f8f6b5a469520edbfd010a73cd4996929cf6318
Shan Shuvo <mrshanshuvo@gmail.com>
2023-02-17 18:38:10 +0600

    basic java learning

---
+++ src/javabegginer/multiplication.java
@@ -0,0 +1,15 @@
+package javabegginer;
+
+import java.util.Scanner;
+
+public class multiplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Enter number for its Multiplication: ");
+        num = input.nextInt();
+        for (int i=1; i<=10; i++){
+            System.out.println(num+" X "+i+" = "+num*i);
+        }
+    }
+}
