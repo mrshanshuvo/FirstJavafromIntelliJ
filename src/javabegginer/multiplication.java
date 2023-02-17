6f582edabb0fe5ef27200c394a8946f4984f3802
Shan Shuvo <mrshanshuvo@gmail.com>
2023-02-17 20:32:32 +0600

    basic java learning

--- src/javabegginer/multiplication.java
+++ src/javabegginer/multiplication.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class multiplication {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num;
-        System.out.print("Enter number for its Multiplication: ");
-        num = input.nextInt();
-        for (int i=1; i<=10; i++){
-            System.out.println(num+" X "+i+" = "+num*i);
+        int num1,num2;
+        System.out.print("Enter the first number for Multiplication: ");
+        num1 = input.nextInt();
+        System.out.println("Enter the last number for Multiplication: ");
+        num2 = input.nextInt();
+        for (int j=num1; j<=num2; j++){
+            for (int i=1; i<=10; i++){
+                System.out.println(j+" X "+i+" = "+j*i);
+            }
+            System.out.println("\n\n");
         }
     }
 }
