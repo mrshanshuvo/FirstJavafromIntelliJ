38fb02bd51bf3f900204f93227c61c5934c1d826
Shan Shuvo <mrshanshuvo@gmail.com>
2023-02-21 13:20:27 +0600

    for each loop : basic java learning

--- src/javabegginer/ArraySorting.java
+++ src/javabegginer/ArraySorting.java
@@ -6,7 +6,10 @@ public class ArraySorting {
     public static void main(String[] args) {
         int [] arr_1d = {27,2,6,1,8,19,34,3556,234};
 
+        String [] name  ={"shahid","hasan","shuvo"};
+
         Arrays.sort(arr_1d);
+        Arrays.sort(name);
 
         for (int j : arr_1d) {
             System.out.println("Ascending: "+j);
@@ -14,6 +17,8 @@ public class ArraySorting {
         for (int i=arr_1d.length-1; i>=0; i-- ){
             System.out.println("Descending: "+arr_1d[i]);
         }
-
+        for (String i: name){
+            System.out.println("Ascending: "+i);
+        }
     }
 }
