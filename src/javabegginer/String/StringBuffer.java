99bb97488e8d4b28e8131af6ba36b093a96ab369
Shan Shuvo <mrshanshuvo@gmail.com>
2023-02-24 12:21:32 +0600

    string basics

---
+++ src/javabegginer/String/StringBuffer.java
@@ -0,0 +1,12 @@
+package javabegginer.String;
+
+public class StringBuffer {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("Shahid");
+        sb.append(" Hasan ");
+        sb.append("Shuvo ");
+        sb.append(25);
+
+        System.out.println(sb);
+    }
+}
